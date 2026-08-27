e109e108a008210173637e21393f7507c9e30375
My Automation <amirsheikh3940@gmail.com>
2026-08-27 12:18:22 -0700

    ok

---
+++ script.js
@@ -0,0 +1,5 @@
+const payload = {
+    topic: document.getElementById('topic').value,
+    page_id: document.getElementById('page_id').value,
+    page_token: document.getElementById('page_token').value
+};
